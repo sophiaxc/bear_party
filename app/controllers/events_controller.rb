8e4b0ae398d8438cef09cc95052042b969f043c0
Sophia Cui <sophiaxc@gmail.com>
2012-07-14 16:32:03 -0700

    Update events api.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,18 @@
 class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
+    latest_id = params[:latest_id]
     if @event
+      latest_id = latest_id ? latest_id : -1
+      event_images =  ActiveRecord::Base.connection.execute("select * from twitters where hashtag='#{@event.hashtag}' and id>#{latest_id} order by twitters.created_at DESC")
+      @event["latest_id"] = latest_id
+
+      if event_images
+        event_images.each { |image| image.delete("image") }
+        @event["images"] = event_images
+      else
+        @event["images"] = nil
+      end
       render :json => @event.to_json
     else
       render :json => { :errors => "No such event!" }, :status => 422
