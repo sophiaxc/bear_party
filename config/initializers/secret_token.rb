c3accd473f2c3b329342f2ab306178788396477d
Sophia Cui <sophiaxc@gmail.com>
2012-07-14 10:11:56 -0700

    Initial bear commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BearParty::Application.config.secret_token = 'a9cd46bb0b004d2bc051c5a999bf9e55e0f23df264001f18733767dbf3f8afb811be3833f30071e7d06dac7e863bac7033e56149630057237b834c3fc97a664c'
